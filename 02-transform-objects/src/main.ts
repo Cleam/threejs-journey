b771b938b62a21af28b5c371c8d8ca2ea9bb11cf
lizhigao <lizhigao@021.com>
2025-05-19 15:10:34 +0800

    feat: transform-objects

--- 02-transform-objects/src/main.ts
+++ 02-transform-objects/src/main.ts
@@ -7,13 +7,66 @@ const canvas = document.querySelector("#webgl") as HTMLCanvasElement;
 const scene = new THREE.Scene();
 
 // objects
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  // wireframe: true,
-});
-const cubeMesh = new THREE.Mesh(geometry, material);
-scene.add(cubeMesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({
+//   color: 0xff0000,
+//   // wireframe: true,
+// });
+// const cubeMesh = new THREE.Mesh(geometry, material);
+// scene.add(cubeMesh);
+
+// // 辅助坐标线
+// const axesHelper = new THREE.AxesHelper(2);
+// scene.add(axesHelper);
+
+// // position the cube
+// // cubeMesh.position.x = 0.7;
+// // cubeMesh.position.y = -0.5;
+// // cubeMesh.position.z = 1;
+// cubeMesh.position.set(0.7, -0.5, 1); // set the position of the cube
+
+// // scale the cube
+// // cubeMesh.scale.x = 2;
+// // cubeMesh.scale.y = 0.5;
+// // cubeMesh.scale.z = 0.5;
+// cubeMesh.scale.set(2, 0.5, 0.5); // set the scale of the cube
+
+// // rotate the cube
+// cubeMesh.rotation.reorder("YXZ"); // 设置旋转顺序
+// // cubeMesh.rotation.x = Math.PI / 4;
+// // cubeMesh.rotation.y = Math.PI / 4;
+// // cubeMesh.rotation.z = Math.PI / 4;
+// cubeMesh.rotation.set(Math.PI / 4, Math.PI / 4, Math.PI / 4); // set the rotation of the cube
+
+// 按组创建对象
+const group = new THREE.Group();
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube1.position.set(0, 0, 0);
+cube2.position.set(1.5, 0, 0);
+cube3.position.set(0, 1.5, 0);
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+scene.add(group);
+// group.position.set(0, 0, 0); // set the position of the group
+// group.scale.set(1, 1, 1); // set the scale of the group
+// group.rotation.set(0, 0, 0); // set the rotation of the group
+// group.rotation.reorder("YXZ"); // 设置旋转顺序
+// group.rotation.set(Math.PI / 4, Math.PI / 4, Math.PI / 4); // set the rotation of the group
+group.rotation.set(0, Math.PI / 4, 0); // set the rotation of the group
+// group.rotation.set(0, 0, Math.PI / 4); // set the rotation of the group
+// group.rotation.set(Math.PI / 4, 0, 0); // set the rotation of the group
 
 // Create a camera
 const sizes = {
@@ -26,7 +79,10 @@ const camera = new THREE.PerspectiveCamera(
   // 0.1,
   // 100
 );
-camera.position.z = 3;
+// camera.position.z = 4; // set the position of the camera
+camera.position.set(0, 0, 4); // set the position of the camera
+// camera.lookAt(0, 0, 0); // 相机朝向原点
+// camera.lookAt(cubeMesh.position); // 相机朝向立方体
 scene.add(camera);
 
 // renderer
